49b610beaf53a98e7c200c357a4a5d1d31786c2c
Niewiaro <kubuskrol@gmail.com>
2024-06-21 23:08:55 +0200

    alias @

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // empty as it's profile page
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
